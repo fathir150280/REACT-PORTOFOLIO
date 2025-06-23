557333bb30ae2a89a83fa77b4ac3ff8e6802ae21
fathir150280 <muhammadfathiralfathsyah2@gmail.com>
2025-06-23 14:46:21 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,151 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Illustrator",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.webp";
+import Proyek2 from "/assets/proyek/proyek2.webp";
+import Proyek3 from "/assets/proyek/proyek3.webp";
+import Proyek4 from "/assets/proyek/proyek4.webp";
+import Proyek5 from "/assets/proyek/proyek5.webp";
+import Proyek6 from "/assets/proyek/proyek6.webp";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Gelar Melayu Serumpun",
+    desk: "Yang diikuti oleh beberapa negara luar seperti Malaysia, Thailand, Singapura, dan Indonesia",
+    dad: "200",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "Penari MALAYSIA",
+    desk: "Berfoto dengan penari dari Malaysia",
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Ronggeng 5 Istana",
+    desk: "kegiatan Ronggeng 5 Istana yang diadakan di Serdang Bedagai",
+    dad: "400",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Seleksi Umum",
+    desk: "Seleksi untuk menentukan siapa yang lanjut tahap berikutnya",
+    dad: "500",
+  },
+  {
+    id: 5,
+    gambar: Proyek5,
+    nama: "Seleksi 10 Besar",
+    desk: "Seleksi sebelum masuk 10 besar",
+    dad: "600",
+  },
+  {
+    id: 6,
+    gambar: Proyek6,
+    nama: "Karantina DAY1",
+    desk: "Lolos babak Seleksi Dan menjalani Karantina hari pertama",
+    dad: "700",
+  },
+];
